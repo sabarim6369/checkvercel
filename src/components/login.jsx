960525f2bf3563fccff33858484a27c34ee18304
sabari <sabarim6369@gmail.com>
2024-09-11 21:58:09 +0530

    vercel cheking

---
+++ src/components/login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import '../css/Login.css';  
+import axios from 'axios';
+import { useNavigate,Link} from 'react-router-dom';
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();  
+    try {
+      const response = await axios.post("https://loginapplicationusingreact.onrender.com/login", { email, password });
+      setMessage(response.data.message);
+      setEmail("");
+      setPassword("");
+      localStorage.setItem("email",email)
+      navigate("/home");
+    } catch (err) {
+      console.error("Error during login:", err.response?.data || err.message);
+      setMessage(err.response?.data?.message || 'An unexpected error occurred');
+    }
+  }
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email:</label>
+            <input 
+              type="email" 
+              placeholder="Enter your email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input 
+              type="password" 
+              placeholder="Enter your password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit" className="login-btn">Login</button>
+          <p>Dont have an account?</p><Link to="/signup">SIGNUP</Link>
+          {message && <p className="message">{message}</p>}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
